1552decfc02261462086c7f579f8c6e5a8ac1548
AdamBenDev <adamaya9732@gmail.com>
2023-04-17 04:12:42 +0200

    files management

---
+++ vite-comics/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+export default {
+  name: 'Dc comics',
+  components: { AppHeader, AppMain, AppFooter }
+}
+</script>
+
+<template>
+
+  <app-header></app-header>
+  <app-main></app-main>
+  <app-footer></app-footer>
+
+</template>
+
+<style lang="scss">
+@use './assets/scss/style.scss';
+</style>
